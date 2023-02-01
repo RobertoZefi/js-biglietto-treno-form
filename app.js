20ceda129e79af032f1226f22624e8de7a2d1797
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-01 19:11:48 +0100

    FEAT start layout

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ console.log(selectKmElement, rangeAgeElement, btnElement)
 btnElement.addEventListener('click', function(){
     console.log('Genera biglietto')
 
-    let priceTicket = selectKmElement.value * priceKm
-    console.log('priceTicket')
+    let priceTicket = parseFloat(selectKmElement.value * priceKm).toFixed(2)
+    console.log(selectKmElement.value, priceTicket)
+
+    let discountMinorenni = 0.2
+    let discountOver65 = 0.4
+    let priceTicketDiscount
+
+    switch (rangeAgeElement.value){
+        case '1':
+            priceTicketDiscount = parseFloat(priceTicket - priceTicket * discountMinorenni).toFixed(2)
+            console.log (priceTicketDiscount)
+            break
+
+        case '2':
+            console.log (priceTicket)
+            break
+        
+        case '3':
+            priceTicketDiscount = parseFloat(priceTicket - priceTicket * discountOver65).toFixed(2)
+            console.log (priceTicketDiscount)
+            break
+    }
 })
